35c9873d21b11378d3394c4846af86600d8ff9ee
Yalcaide <yalcaide@uade.edu.ar>
2025-12-19 03:57:02 -0300

    Add files via upload

---
+++ diseñoweb/javascript/script.js
@@ -0,0 +1,5 @@
+const titulo= document.createElement ("h1")
+titulo.textContent = "Hola a a a a a a a a a"
+const div= document.querySelector("div")
+div.append(titulo)
+titulo.classList.add("color")
